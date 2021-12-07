06c88a9519218b41a60161a524908c01df495e59
Diego <newskyforge2@hotmail.com>
2021-12-06 21:17:43 -0600

    rename dist folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     assetModuleFilename: 'images/[name][hash][ext][query]',
   },
   devtool: 'inline-source-map',
