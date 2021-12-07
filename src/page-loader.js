b20566f3fa7644cc902282817f3e54f130a207f1
Diego <newskyforge2@hotmail.com>
2021-12-06 18:25:55 -0600

    Add menu page loader

--- src/page-loader.js
+++ src/page-loader.js
@@ -1,11 +1,17 @@
 import createHomePage from './home-page';
+import createMenuPage from './menu-page';
+
 const homePage = createHomePage();
+const menuPage = createMenuPage();
 
 function loadPage(page, container) {
   container.firstChild?.remove();
   switch (page) {
     case 'home':
       container.appendChild(homePage);
+      break;
+    case 'menu':
+      container.appendChild(menuPage);
       break;
   }
 }
